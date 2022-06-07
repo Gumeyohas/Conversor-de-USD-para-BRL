6c0df1fd66868ee269177b7201777aeb53b0b19e
Gumeyohas <79321718+Gumeyohas@users.noreply.github.com>
2022-06-07 14:15:17 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Conversor de moedas
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Freire-gus/pen/WNRverE](https://codepen.io/Freire-gus/pen/WNRverE).
+
---
+++ script.js
@@ -0,0 +1,12 @@
+var valorEmDolar = parseFloat(prompt("Qual o valor em dolar que você quer converter?"))
+
+var valorEmReal = valorEmDolar * 5.50
+var valorEmRealFixado = valorEmReal.toFixed(2)
+
+alert(valorEmRealFixado)
+
+//Revisão
+//Variáveis var int - flat - string
+//alert - parseInt - parseFloat - prompt
+// operações + somar * multiplicar
+// comentário final
